aaea31f76b0390d45abd9271dd3f4e725c6fbdad
sanchit2843 <sanchittanwar75@gmail.com>
2019-07-04 04:25:53 +0530

    old files removed

--- plot.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-data = pd.read_csv('C:/Users/sanch/Downloads/genre_4_35.csv')
-data1 = data.iloc[:35,1:].values
-import matplotlib.pyplot as plt
-plt.plot(data1[:,0], data1[:,1])
-plt.show()
